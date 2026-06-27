98a654aefeb466e4cf5bb5fcb3c174a31e6d2c71
hammoudildm-crypto <hammoudi.ldm@gmail.com>
2026-06-27 14:17:41 +0100

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,12 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import Referentiels from '../views/Referentiels.vue'
+import PlanDirecteur from '../views/PlanDirecteur.vue'
 
 const routes = [
   { path: '/', name: 'home', component: Home },
   { path: '/referentiels', name: 'referentiels', component: Referentiels },
+  { path: '/plan', name: 'plan', component: PlanDirecteur },
   { path: '/login', name: 'login', component: Login },
 ]
 
