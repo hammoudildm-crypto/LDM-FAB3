dd683f37b337bd251cf177448cdb80eb1690c998
hammoudildm-crypto <hammoudi.ldm@gmail.com>
2026-06-28 09:27:50 +0100

    index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,15 +3,16 @@ import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import Referentiels from '../views/Referentiels.vue'
 import PlanDirecteur from '../views/PlanDirecteur.vue'
+import OrdresFabrication from '../views/OrdresFabrication.vue'
 
 const routes = [
   { path: '/', name: 'home', component: Home },
   { path: '/referentiels', name: 'referentiels', component: Referentiels },
   { path: '/plan', name: 'plan', component: PlanDirecteur },
+  { path: '/ordres', name: 'ordres', component: OrdresFabrication },
   { path: '/login', name: 'login', component: Login },
 ]
 
-// Hash mode (#/route) : évite les 404 au refresh sur GitHub Pages.
 export default createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
