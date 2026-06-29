4aa748b6698f7ef0ac7122023d174f6d5f99c07e
hammoudildm-crypto <hammoudi.ldm@gmail.com>
2026-06-29 19:55:18 +0100

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ provide('peutEditer', peutEditer)
 const roleLabel = computed(() => ({ admin: 'Admin', operateur: 'Opérateur', lecteur: 'Lecteur' }[role.value] || ''))
 
 const PROD = ['/plan', '/ordres', '/suivi', '/encours', '/conditionnement', '/dossier']
-const PILOT = ['/ca', '/realisation-plan', '/effectifs', '/verification-ddl', '/audit', '/habilitations']
+const PILOT = ['/ca', '/realisation-plan', '/rendement', '/effectifs', '/verification-ddl', '/audit', '/habilitations']
 const prodActive = computed(() => PROD.includes(route.path))
 const pilotActive = computed(() => PILOT.includes(route.path))
 
@@ -99,6 +99,7 @@ async function signOut() {
           <div v-show="openMenu === 'pilotage'" class="drop-panel">
             <RouterLink to="/ca" @click="closeMenu">Chiffre d'affaires</RouterLink>
             <RouterLink to="/realisation-plan" @click="closeMenu">Réalisation vs Plan</RouterLink>
+            <RouterLink to="/rendement" @click="closeMenu">Rendement</RouterLink>
             <RouterLink to="/effectifs" @click="closeMenu">Effectifs</RouterLink>
             <RouterLink to="/verification-ddl" @click="closeMenu">Vérification DDL</RouterLink>
             <RouterLink to="/audit" @click="closeMenu">Journal d'audit</RouterLink>
